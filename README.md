00eba73c1d0a6139c2658fadbe45c8d9f6f68796
Shreyansh Agarwal <shr818bytes@gmail.com>
2021-12-15 01:19:22 +0530

    Update home page

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# demoplix1
+# Marine Palms Resort Web App
+## deployed at: demoplix1.herokuapp.com
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import requests
+
 app = Flask(__name__)
 
 # To run python file without re-running the flask command
@@ -10,7 +12,16 @@ app.before_request(before_request)
 # Routes
 @app.route('/')  
 def home():
-    return render_template('home.html')
+    # fetch username
+    first_name = ''
+    try:
+        session_id = request.args.get('session_id')
+        raw_url = "https://micro-okta.herokuapp.com/api/name?session_id=%s"%(session_id)
+        username_re = requests.get(raw_url)
+        first_name, *rest_username = username_re.text.split()
+    except Exception as e:
+        print(e)
+    return render_template('home.html', username=first_name)
 
 @app.route('/login')
 def login():
